6eda1469acee0d33d9164687123228e53e0886e6
David A. Parry <david.parry@ed.ac.uk>
2019-01-05 17:21:16 +0000

    prevent downloading of non-existent tier-5 data

--- data/download.pl
+++ data/download.pl
@@ -6,7 +6,7 @@ foreach my $y (1993..2018){
     my $x = $y + 1; 
     my $season = substr($y, 2) . substr($x, 2); 
     print "mkdir -p $y && cd $y\n"; 
-    foreach my $div (0..4){ 
+    foreach my $div (0..3){ 
         print "wget http://www.football-data.co.uk/mmz4281/$season/E$div.csv\n";
     }
     print "cd ../\n";
